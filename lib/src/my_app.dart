ac76f83d1b2a7bfb4033ea0c6194f02357474ecd
Lam Nguyen <lamnguyen1112@gmail.com>
2023-08-02 14:49:49 +0700

    add splash screen

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:component_library/component_library.dart';
 import 'package:my_flutter_app/src/main_tab_screen.dart';
+import 'package:my_flutter_app/src/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       localizationsDelegates: ComponentLocalizations.localizationsDelegates,
       supportedLocales: ComponentLocalizations.supportedLocales,
-      locale: Locale('en'),
-      home: MainTabScreen(),
+      locale: const Locale('en'),
+      // home: MainTabScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => const MainTabScreen(),
+      },
     );
   }
 }
---
+++ lib/src/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
